105107927f37bcd466d76d67f9b28c60a30f17bd
Qadir Hussain <qadir.hussain99@gmail.com>
2022-04-23 22:13:52 +0100

    Adding material ui libraries

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import PhotoCamera from '@material-ui/icons';
+
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Welcome to ReactJS!!</Typography>
+        </div>
+    )
+}
+
+export default App;
