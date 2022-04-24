a8a188af430e10db7ce971d63da7a59809e51507
Qadir Hussain <qadir.hussain99@gmail.com>
2022-04-24 09:20:37 +0100

    Updating App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
-import PhotoCamera from '@material-ui/icons';
+// import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+// import PhotoCamera from '@material-ui/icons';
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 
 const App = () => {
     return (
-        <div>
-            <Typography variant="h1">Welcome to ReactJS!!</Typography>
+        <div className="App">
+            <header className="App-header">
+                <ButtonGroup variant="contained" color="primary">
+                    <Button startIcon={<SaveIcon />}>
+                        Welcome to ReactJS
+                    </Button>
+                    <Button startIcon={<DeleteIcon />}>
+                        Welcome to ReactJS
+                    </Button>
+                </ButtonGroup>
+                
+            </header>
         </div>
     )
 }
